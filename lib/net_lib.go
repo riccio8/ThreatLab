1dcf54edec99e4fd7bb78a05a04c1474178915d3
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-09 16:35:34 +0200

    Update net_lib.go

--- lib/net_lib.go
+++ lib/net_lib.go
@@ -1,26 +1,41 @@
-package main
+package lib
 
 import (
-	"fmt"
 	"net"
 	"strconv"
+	"fmt"
 	"time"
 )
+type ScanResult struct{
+	Port int
+	State string
+}
 
-func scanPort(protocol, hostname string, port int) bool{
+
+func scanPort(protocol, hostname string, port int) ScanResult{
+	result = ScanResult{Port: port}
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
 
 	if err != nil{
-		return false
+		result.State = "Cloesd"
+		return result
 	}
 
 	defer conn.Close()
-	return true
+	result.State = "Open"
+	return result
+
+	fmt.Println(Port, "\n", port, "\n", result)
 }
 
-func main(){
-	fmt.Println("Port scanning")
-	open := scanPort("tcp", "localhost", 80)
-	fmt.Println("Port status:\n", open)
+func InintialScan(hostname string) []ScanResult {
+	var results []ScanResult
+
+	for i := 0, i <= 1024; 1++{
+		results = append (results, Scanport("tcp", hostname, i))
+	}
+
+	return results
+
 }
