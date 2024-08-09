0e679cf5808a5f8a6e98fdc6c23014d5b9988986
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-09 16:09:53 +0200

    Update net_lib.go

--- lib/net_lib.go
+++ lib/net_lib.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func scanPort(protocol, hostname str, port int) bool{
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second())
+
+	if err != nil{
+		return false
+	}
+
+	defer conn.Close()
+	return true
+}
+
+func main(){
+	fmt.Println("Port scanning")
+	open := scanPort("tcp", "localhost", 80)
+	fmt.Println("Port status: %t\n", open)
+}
