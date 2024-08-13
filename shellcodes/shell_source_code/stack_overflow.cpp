d5681b92770e41cac44057c3ac6ea840ebe9f77c
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-13 13:00:04 +0200

    Update stack_overflow.cpp

--- shellcodes/shell_source_code/stack_overflow.cpp
+++ shellcodes/shell_source_code/stack_overflow.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
 #include <string.h>
 using namespace std;
 
 void function(char *input){
-  char buffer[1];
-  strcpy(buffer, input);
+    char buffer[1];
+    strcpy(buffer, input); 
+}
+
+int main(){
+    const char *string = "hello world";
+    function((char *)string); 
+    return 0;
 }
