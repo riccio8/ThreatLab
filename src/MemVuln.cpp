cddc854ab56064bd187efcc0bd59264532248f8f
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-16 16:19:10 +0200

    Update MemVuln.cpp

--- src/MemVuln.cpp
+++ src/MemVuln.cpp
@@ -2,54 +2,54 @@
 
 using namespace std;
 
-class MemType {
-public:
-    long number;
-    char* buffer;
 
-    MemType(long n, const char* b) {
-        this->number = n;
-        this->buffer = new char[strlen(b) + 1];
-        strcpy(this->buffer, b);
-    }
+MemType::MemType(long n, const char* b) {
+    this->number = n;
+    this->buffer = new char[strlen(b) + 1];
+    strcpy(this->buffer, b);
+}
 
-    ~MemType() {
-        delete[] buffer;
-    }
 
-    void MemLeak() {
-        char buffer[2];
-        strcpy(buffer, "TooLargeForBuffer");
-        cout << "Buffer contains: " << sizeof(buffer) << endl;
-    }
+MemType::~MemType() {
+    delete[] buffer;
+}
 
-    void heapLeak() {
-        long* num1 = new long(1000000000000);
-        long num2 = (*num1) * 10000;
-        cout << "Result of heap operation: " << num2 << endl;
-    }
-};
 
-class VulnerableVoid {
-public:
-    void stack_overflow(const char* input) {
-        char buffer[8];  
-        cout << "Simulating a stack overflow: " << endl;
-        strcpy(buffer, input);  
-        cout << "Data in the buffer: " << buffer << endl;
-    }
+void MemType::MemLeak() {
+    char buffer[2];
+    strcpy(buffer, "TooLargeForBuffer");
+    cout << "Buffer contains: " << buffer << endl;
+}
 
-    void heap_overflow(const char* big) {
-        cout << "Simulating heap overflow..." << endl;
-        char* buffer1 = new char[4];
-        char* buffer2 = new char[64];
 
-        strcpy(buffer1, big); 
-        cout << "Buffer1: " << buffer1 << endl;
-        cout << "Buffer2: " << buffer2 << endl;
-        
-    }
-};
+void MemType::heapLeak() {
+    long* num1 = new long(1000000000000);
+    long num2 = (*num1) * 10000;
+    cout << "Result of heap operation: " << num2 << endl;
+    delete num1; // Ricordati di liberare la memoria
+}
+
+
+void VulnerableVoid::stack_overflow(const char* input) {
+    char buffer[8];  
+    cout << "Simulating a stack overflow: " << endl;
+    strcpy(buffer, input);  
+    cout << "Data in the buffer: " << buffer << endl;
+}
+
+
+void VulnerableVoid::heap_overflow(const char* big) {
+    cout << "Simulating heap overflow..." << endl;
+    char* buffer1 = new char[4];
+    char* buffer2 = new char[64];
+
+    strcpy(buffer1, big); 
+    cout << "Buffer1: " << buffer1 << endl;
+    cout << "Buffer2: " << buffer2 << endl;
+
+    delete[] buffer1; // Liberare la memoria
+    delete[] buffer2; // Liberare la memoria
+}
 
 int main() {
     VulnerableVoid vul;
