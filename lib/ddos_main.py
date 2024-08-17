be778441b864474c51c392c7227db9a6b1830d53
Riccardo <134144784+riccio8@users.noreply.github.com>
2024-08-17 16:28:50 +0200

    Update ddos_main.py

--- lib/ddos_main.py
+++ lib/ddos_main.py
@@ -5,7 +5,7 @@ import sys
 import os
 from scapy.all import IP, TCP, send, RandShort
 import random
-import dos_lib as ty
+import lib/dos_lib as ty
 import platform
 import ping3
 from scapy.all import *
