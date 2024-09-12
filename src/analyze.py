264addcd8befa8195fb130b4a00761093a5ff258
0x90 <134144784+riccio8@users.noreply.github.com>
2024-09-12 15:37:19 +0200

    Update analyze.py

--- src/analyze.py
+++ src/analyze.py
@@ -1,5 +1,7 @@
 import os
 import platform
+import pefile as pe
+import peutils as pes
 
 if platform.system == 'Linux':
     os.system("pip3 install psutil")
@@ -8,10 +10,37 @@ else:
   os.system("pip install psutil")
   import psutil
 
-proc_name = input("type the name of the process u want to analyze, like notepad.exe: \n")
+
+proc_name = input("[X]name of the process...: \n")
+
+
+def get_process_path(pid):
+    try:
+        process = psutil.Process(pid)
+        return process.exe()  
+    except psutil.NoSuchProcess:
+        return f"Process with PID {pid} not found."
+    except psutil.AccessDenied:
+        return "AccessDenied to the proces."
+    except Exception as e:
+        return str(e)
+
 
 for processes in psutil.process_iter():
     if processes.name() == proc_name:
-        print(processes, "with pid: \n", processes.pid)
+        print("[INFO]: ", processes, "with pid: \n", processes.pid)
         process_pid = processes.pid
-        end_process = os.system("taskkill /in" + str(processes.pid))
+        kill = input("[*] Do u want to kill the process? (Y/N): \n")
+        if kill == lower("Y")
+            end_process = os.system("taskkill /in" + str(processes.pid))
+        else:
+            print("[X] Analysis of", proc_name, ".")
+            print("[X] Analysis could take some time, please wait...")
+            time.sleep(2)
+            print("[X] Analysis in progress...")
+            path = get_process_path(process_pid)
+            pes.peutils.is_suspicious(path)
+            
+else: 
+    print("[x]No processes found...")
+
