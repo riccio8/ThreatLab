84488ad0e47f17d3a22a6a186d1cbce267ad342b
0x90 <134144784+riccio8@users.noreply.github.com>
2024-09-12 15:21:15 +0200

    Create analyze.py

---
+++ src/analyze.py
@@ -0,0 +1,15 @@
+import os
+import platform
+
+if platform.system == 'Linux':
+    os.system("pip3 install psutil")
+    import psutil
+else: 
+  os.system("pip install psutil")
+  import psutil
+
+proc_name = input("type the name of the process u want to analyze, like notepad.exe: \n")
+
+for processes in psutil.process_iter():
+    if processes.name() == proc_name:
+        print(process)
