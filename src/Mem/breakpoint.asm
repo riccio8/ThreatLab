f82e03661ab14c47647b745a1b25b26687f55853
0x90 <134144784+riccio8@users.noreply.github.com>
2024-09-13 17:47:15 +0200

    Update breakpoint.asm

--- src/Mem/breakpoint.asm
+++ src/Mem/breakpoint.asm
@@ -5,3 +5,15 @@
     movq %rsp, %rbp
 
     movq %dr7, %rax
+    testq $0xFF, %rax
+    jz no_breakpoint
+
+    movl $1, %eax
+    jmp end
+
+no_breakpoint:
+    xorl %eax, %eax
+
+end:
+    popq %rbp
+    ret
