a703a5c4d045879afbab10c23e5fe631b10140b4
0x90 <134144784+riccio8@users.noreply.github.com>
2024-09-14 09:49:14 +0200

    Add files via upload

---
+++ lib/port_lib.go
@@ -0,0 +1,22 @@
+package port
+
+import (
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+func ScanPort(protocol, hostname string, port int) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeOut(protocol, address, 60*time.Second)
+
+	if err != nil {
+		log.Fatal("[ERROR] Port is close... ", err)
+		return false 
+	}
+
+	defer conn.Close()
+
+	return true
+}
