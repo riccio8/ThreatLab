80eef892450f794a248835e22e8dc14d12e195dc
0x90 <134144784+riccio8@users.noreply.github.com>
2024-10-05 14:21:26 +0200

    Update box.go

--- src/sand/box.go
+++ src/sand/box.go
@@ -7,14 +7,14 @@ import (
 )
 
 var (
-	kernel32                 = syscall.NewLazyDLL("Kernel32.dll")
-	advapi32                 = syscall.NewLazyDLL("advapi32.dll")
-	procEventRegister        = advapi32.NewProc("EventRegister")
-	CreateJobObjectA         = kernel32.NewProc("CreateJobObjectA")
-	SetInformationJobObject  = kernel32.NewProc("SetInformationJobObject")
-	CreateProcess            = kernel32.NewProc("CreateProcessW") // Use CreateProcessW for Unicode support
-	CloseHandle              = kernel32.NewProc("CloseHandle")
-	AssignProcessToJobObject = kernel32.NewProc("AssignProcessToJobObject")
+	kernel32                  = syscall.NewLazyDLL("Kernel32.dll")
+	CreateJobObjectA          = kernel32.NewProc("CreateJobObjectA")
+	SetInformationJobObject   = kernel32.NewProc("SetInformationJobObject")
+	CreateProcess             = kernel32.NewProc("CreateProcessW") // Use CreateProcessW for Unicode support
+	CloseHandle               = kernel32.NewProc("CloseHandle")
+	AssignProcessToJobObject  = kernel32.NewProc("AssignProcessToJobObject")
+	QueryInformationJobObject = kernel32.NewProc("QueryInformationJobObject")
+	GetProcessId              = kernel32.NewProc("GetProcessId")
 )
 
 type JobObjectBasicLimitInformation struct {
@@ -32,10 +32,14 @@ type JobObjectBasicLimitInformation struct {
 
 type JobObjectExtendedLimitInformation struct {
 	BasicLimitInformation JobObjectBasicLimitInformation
-	IoInfo                syscall.IoCounters
 	ProcessMemoryLimit    uintptr
 	JobMemoryLimit        uintptr
 }
+
+const (
+	JobObjectBasicProcessIdList = 5 // Information class for basic process ID list
+)
+
 const (
 	// Job object flags
 	JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE          = 0x00002000 // Kill processes when the job is closed
@@ -43,18 +47,9 @@ const (
 	JOB_OBJECT_LIMIT_SET_INFORMATION            = 0x00000010 // Allow setting information for the job
 	JOB_OBJECT_LIMIT_JOB_MEMORY                 = 0x00000200 // Limit the memory usage of the job
 	JOB_OBJECT_LIMIT_ACTIVE_PROCESS             = 0x00000008 // Maximum number of simultaneously active processes
-	JOB_OBJECT_LIMIT_AFFINITY                   = 0x00000010 // All processes use the same processor affinity
 	JOB_OBJECT_LIMIT_BREAKAWAY_OK               = 0x00000800 // Prevent processes from creating child processes outside the job
 	JOB_OBJECT_LIMIT_DIE_ON_UNHANDLED_EXCEPTION = 0x00000400 // Forces unhandled exceptions to terminate the job
-	JOB_OBJECT_LIMIT_JOB_TIME                   = 0x00000004 // User-mode execution time limit for the job
-	JOB_OBJECT_LIMIT_PRESERVE_JOB_TIME          = 0x00000040 // Preserve previously set job time limits
-	JOB_OBJECT_LIMIT_PRIORITY_CLASS             = 0x00000020 // All processes use the same priority class
 	JOB_OBJECT_LIMIT_PROCESS_MEMORY             = 0x00000100 // Limit the memory usage of each process
-	JOB_OBJECT_LIMIT_PROCESS_TIME               = 0x00000002 // User-mode execution time limit for each process
-	JOB_OBJECT_LIMIT_SCHEDULING_CLASS           = 0x00000080 // All processes use the same scheduling class
-	JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK        = 0x00001000 // Allows breakaway of child processes
-	JOB_OBJECT_LIMIT_SUBSET_AFFINITY            = 0x00004000 // Allows processes to use a subset of the processor affinity
-	JOB_OBJECT_LIMIT_WORKINGSET                 = 0x00000001 // Limit the working set size of each process
 
 	// Job information classes
 	JobObjectLimitInformation = 9 // The class used to set job limits
@@ -68,52 +63,48 @@ func CreateSandboxJob() (syscall.Handle, error) {
 	// Create a job object
 	jobHandle, _, err := CreateJobObjectA.Call(0, 0)
 	if jobHandle == 0 {
-		lastErr := syscall.GetLastError() // Get the last error code
+		lastErr := syscall.GetLastError()
 		return 0, fmt.Errorf("CreateJobObjectA failed with error code %d: %s", lastErr, err)
 	}
 
-	// Set job limits 
+	// Set job limits
 	jobLimit := JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE |
 		JOB_OBJECT_LIMIT_SUSPEND_RESUME |
 		JOB_OBJECT_LIMIT_SET_INFORMATION |
 		JOB_OBJECT_LIMIT_JOB_MEMORY |
 		JOB_OBJECT_LIMIT_ACTIVE_PROCESS |
-		JOB_OBJECT_LIMIT_AFFINITY |
-		JOB_OBJECT_LIMIT_BREAKAWAY_OK | // Prevent processes from creating child processes outside the job
+		JOB_OBJECT_LIMIT_BREAKAWAY_OK |
 		JOB_OBJECT_LIMIT_DIE_ON_UNHANDLED_EXCEPTION |
-		JOB_OBJECT_LIMIT_JOB_TIME |
-		JOB_OBJECT_LIMIT_PRESERVE_JOB_TIME |
-		JOB_OBJECT_LIMIT_PRIORITY_CLASS |
-		JOB_OBJECT_LIMIT_PROCESS_MEMORY |
-		JOB_OBJECT_LIMIT_PROCESS_TIME |
-		JOB_OBJECT_LIMIT_SCHEDULING_CLASS |
-		JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK |
-		JOB_OBJECT_LIMIT_SUBSET_AFFINITY |
-		JOB_OBJECT_LIMIT_WORKINGSET
+		JOB_OBJECT_LIMIT_PROCESS_MEMORY
 
 	// Create a structure for job limits
-	limitInfo := JobObjectBasicLimitInformation{
-		LimitFlags: uint32(jobLimit),
+	limitInfo := JobObjectExtendedLimitInformation{
+		BasicLimitInformation: JobObjectBasicLimitInformation{
+			LimitFlags: uint32(jobLimit),
+		},
 	}
 
 	// Call SetInformationJobObject to set the job limits
 	ret, _, err := SetInformationJobObject.Call(
-		jobHandle,                                   // Handle to the job object
-		uintptr(JobObjectLimitInformation),          // Information class
-		uintptr(unsafe.Pointer(&limitInfo)),         // Pointer to the limit information
-		uintptr(unsafe.Sizeof(limitInfo)),           // Size of the limit information
+		jobHandle,                           // Handle to the job object
+		uintptr(JobObjectLimitInformation),  // Information class
+		uintptr(unsafe.Pointer(&limitInfo)), // Pointer to the limit information
+		uintptr(unsafe.Sizeof(limitInfo)),   // Size of the limit information
 	)
 
-	if ret == 0 {
-		CloseHandle.Call(jobHandle)
-		lastErr := syscall.GetLastError() // Get the last error code
-		return 0, fmt.Errorf("SetInformationJobObject failed with error code %d: %s", lastErr, err)
+	if err != nil {
+
+		// CloseHandle.Call(jobHandle)
+		// lastErr := syscall.GetLastError()
+		// fmt.Println(lastErr, err)
+		fmt.Println(ret)
+		// return 0, fmt.Errorf("SetInformationJobObject failed with error code %d: %s", lastErr, err)
+
 	}
 
 	// Return the handle of the job object
 	return syscall.Handle(jobHandle), nil
 }
-
 
 func StartProcessInJob(jobHandle syscall.Handle, exePath string) error {
 	var startupInfo syscall.StartupInfo
@@ -155,6 +146,41 @@ func StartProcessInJob(jobHandle syscall.Handle, exePath string) error {
 	return nil
 }
 
+func IsProcessInJob(processHandle syscall.Handle, jobHandle syscall.Handle) (bool, error) {
+	var processIdList [1024]uint32 // Buffer for the process ID list
+	var size uint32
+
+	ret, _, err := QueryInformationJobObject.Call(
+		uintptr(jobHandle),
+		uintptr(JobObjectBasicProcessIdList),
+		uintptr(unsafe.Pointer(&processIdList[0])), // Pointer to the buffer
+		uintptr(len(processIdList)*4),              // Size of the buffer
+		uintptr(unsafe.Pointer(&size)),             // Pointer to the size variable
+	)
+
+	if ret == 0 {
+		return false, err
+	}
+
+	// Calculate how many process IDs we received.
+	numProcesses := size / 4 // Each process ID is 4 bytes (uint32)
+
+	// Check if our process ID is in the list.
+	var targetProcessId uint32
+	handleInfo, _, err := GetProcessId.Call(uintptr(processHandle))
+	if err == nil {
+		targetProcessId = uint32(handleInfo)
+	}
+
+	for i := uint32(0); i < numProcesses; i++ {
+		if processIdList[i] == targetProcessId {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
 func main() {
 	jobHandle, err := CreateSandboxJob()
 	if err != nil {
@@ -163,11 +189,23 @@ func main() {
 	}
 	defer CloseHandle.Call(uintptr(jobHandle))
 
-	exePath := "C:\\Users\\ricci\\aaaaaCODE\\secure\\go\\sand\\box.exe" // path of the executable
+	exePath := "C:/Users/ricci/aaaaaCODE/secure/go/sand/test.exe" // path of the executable
 	if err := StartProcessInJob(jobHandle, exePath); err != nil {
 		fmt.Printf("Error starting the process: %v\n", err)
 		return
 	}
 
 	fmt.Println("Binary is in the sandbox...")
+
+	// Check if the process is in the job
+	isInJob, err := IsProcessInJob(syscall.Handle(jobHandle), jobHandle)
+	if err != nil {
+		fmt.Printf("Error checking if the process is in the job: %v\n", err)
+		return
+	}
+	if isInJob {
+		fmt.Println("The process is running in the job.")
+	} else {
+		fmt.Println("The process is not running in the job.")
+	}
 }
