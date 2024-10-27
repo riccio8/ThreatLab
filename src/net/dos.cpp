fe7ab77d62163e3b6501d915f093ab0504ea1883
0x90 <134144784+riccio8@users.noreply.github.com>
2024-10-27 18:31:21 +0100

    Update dos.cpp
    implemented connection

--- src/net/dos.cpp
+++ src/net/dos.cpp
@@ -2,97 +2,77 @@
 #define UNICODE 1
 #endif
 
-#include <iostream>
+#include <winsock2.h>  // Include winsock2.h before windows.h
 #include <windows.h>
-#include <winsock2.h>
 #include <ws2tcpip.h>
+#include <iostream>
 #include <stdio.h>
 
-#pragma comment(lib, "ws2_32.lib")  // Required for linking with Ws2_32.lib
-
-// #define SERVER "localhost"  
-// #define BUFFER_SIZE 1024  
 #define PORT 8080
-#define SOCK_STREAM 1
-#define AF_INET 2
-#define IPPROTO_TCP 6
-
 #define WIN32_LEAN_AND_MEAN
 
-
-int main()
-{
-
-    WORD wVersionRequested;
+int main() {
+    WORD wVersionRequested = MAKEWORD(2, 2);
     WSADATA wsaData;
     int err;
 
-/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
-    wVersionRequested = MAKEWORD(2, 2);
-
     err = WSAStartup(wVersionRequested, &wsaData);
     if (err != 0) {
-        /* Tell the user that we could not find a usable */
-        /* Winsock DLL.                                  */
-        std::cerr << "WSAStartup failed with error: \n" << err << std::endl;
+        std::cerr << "WSAStartup failed with error: " << err << std::endl;
         return 1;
     }
 
     if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
-        /* Tell the user that we could not find a usable */
-        /* WinSock DLL.                                  */
         std::cerr << "Could not find a usable version of Winsock.dll" << std::endl;
         WSACleanup();
         return 1;
-    }
-    else
+    } else {
         std::cout << "The Winsock 2.2 dll was found okay" << std::endl;
-        
-        
-        
-    int iResult = 0;
-
-//    int i = 1;
-
-    SOCKET sock = INVALID_SOCKET;
-    int iFamily = AF_UNSPEC;
-    int iType = 0;
-    int iProtocol = 0;
-
-// The Winsock DLL is acceptable. Proceed to use it. 
-    
-    sock = socket(iFamily, iType, iProtocol);
-    if (sock == INVALID_SOCKET){
-        std::cerr << L"socket function failed with error \n" <<  WSAGetLastError() << std::endl;
     }
-    else {
-        std::cout << L"socket function succeeded\n" << std::endl;
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket function failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    } else {
+        std::cout << "Socket function succeeded" << std::endl;
     }
     
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(80); // Port 80 
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); 
     
-// SHUTTING DOWN CONNECTION AND CLOSING SOCKET
     
-    iResult = shutdown(sock, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        std::cerr << L"shutdown failed with error \n" << WSAGetLastError() << std::endl;
+    int connectResult = connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (connectResult == SOCKET_ERROR) {
+        std::cerr << "Error during connection: " << WSAGetLastError() << std::endl;
+        std::cerr << GetLastError() << std::endl;
         closesocket(sock);
         WSACleanup();
         return 1;
     }
 
-    closesocket(sock);
     
-    std::cout << L"shutdown and closesocket function passed\n" << std::endl;
-    
-    
-        
-/*Then call WSACleanup when done using the Winsock dll */
-    
-    WSACleanup();
-    
-    return 0;
+    // Shutting down connection and closing socket
+    int iResult = shutdown(sock, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        std::cerr << "Shutdown failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    } else {
+        std::cout << "No shutdown requested" << std::endl;
+    }
 
+    closesocket(sock);
+    std::cout << "Shutdown and closesocket function passed" << std::endl;
+
+    WSACleanup();
+    return 0;
 }
+
 
 /*  THIS IS JUST THE FIRST PART, THE INITIAL SETUP, I'LL ADD THREAD AND SO ON, JUST WAIT */
 
