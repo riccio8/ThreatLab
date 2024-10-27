801796ac04690b61bed0947d7616473f239d295f
0x90 <134144784+riccio8@users.noreply.github.com>
2024-10-27 11:43:54 +0100

    Update dos_Prog.cpp
    setup done, need to implement the connection and later the threadssss

--- src/net/dos_Prog.cpp
+++ src/net/dos_Prog.cpp
@@ -1,3 +1,7 @@
+#ifndef UNICODE
+#define UNICODE 1
+#endif
+
 #include <iostream>
 #include <windows.h>
 #include <winsock2.h>
@@ -6,6 +10,8 @@
 
 #pragma comment(lib, "ws2_32.lib")  // Required for linking with Ws2_32.lib
 
+// #define SERVER "localhost"  
+// #define BUFFER_SIZE 1024  
 #define PORT 8080
 #define SOCK_STREAM 1
 #define AF_INET 2
@@ -42,15 +48,50 @@ int main()
     else
         std::cout << "The Winsock 2.2 dll was found okay" << std::endl;
         
+        
+        
+    int iResult = 0;
 
-/* The Winsock DLL is acceptable. Proceed to use it. */
+//    int i = 1;
 
+    SOCKET sock = INVALID_SOCKET;
+    int iFamily = AF_UNSPEC;
+    int iType = 0;
+    int iProtocol = 0;
 
-/* then call WSACleanup when done using the Winsock dll */
+// The Winsock DLL is acceptable. Proceed to use it. 
+    
+    sock = socket(iFamily, iType, iProtocol);
+    if (sock == INVALID_SOCKET){
+        std::cerr << L"socket function failed with error \n" <<  WSAGetLastError() << std::endl;
+    }
+    else {
+        std::cout << L"socket function succeeded\n" << std::endl;
+    }
+    
+    
+// SHUTTING DOWN CONNECTION AND CLOSING SOCKET
+    
+    iResult = shutdown(sock, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        std::cerr << L"shutdown failed with error \n" << WSAGetLastError() << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(sock);
+    
+    std::cout << L"shutdown and closesocket function passed\n" << std::endl;
+    
+    
+        
+/*Then call WSACleanup when done using the Winsock dll */
     
     WSACleanup();
     
     return 0;
+
 }
 
 /*  THIS IS JUST THE FIRST PART, THE INITIAL SETUP, I'LL ADD THREAD AND SO ON, JUST WAIT */
