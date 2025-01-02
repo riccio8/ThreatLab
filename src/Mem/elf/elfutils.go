d007709e39ac2902645be5b4e61c25764211ce72
0x90 <134144784+riccio8@users.noreply.github.com>
2025-01-02 20:54:26 +0100

    Update elfutils.go

--- src/Mem/elf/elfutils.go
+++ src/Mem/elf/elfutils.go
@@ -4,7 +4,6 @@
  */
 
 
-
 package main
 
 import (
@@ -14,6 +13,7 @@ import (
 	"fmt"
 	"os"
 )
+
 
 func load(fileName string) (*elf.File, error) {
 	file, err := elf.Open(fileName)
@@ -29,6 +29,35 @@ func DWARF(f *elf.File) (*dwarf.Data, error) {
 		return nil, err
 	}
 	return data, nil
+}
+
+
+func processStringTags(elfFile *elf.File, tags []elf.DynTag) []any {
+
+	var results []any
+	for _, tag := range tags {
+		values, err := elfFile.DynString(tag)
+		if err != nil {
+			results = append(results, fmt.Sprintf("Error reading tag %v: %v", tag, err))
+			continue
+		}
+		results = append(results, map[string]any{fmt.Sprintf("%v", tag): values})
+	}
+	return results
+}
+
+
+func processValueTags(elfFile *elf.File, tags []elf.DynTag) []any {
+	var results []any
+	for _, tag := range tags {
+		values, err := elfFile.DynValue(tag)
+		if err != nil {
+			results = append(results, fmt.Sprintf("Error reading tag %v: %v", tag, err))
+			continue
+		}
+		results = append(results, map[string]any{fmt.Sprintf("%v", tag): values})
+	}
+	return results
 }
 
 func sections(f *elf.File) any {
@@ -169,6 +198,8 @@ func help() {
 	fmt.Printf("  %sdynamicSymbols%s - Show dynamic symbols in the ELF file.\n", green, reset)
 	fmt.Printf("  %srelocs%s         - List relocation entries.\n", green, reset)
 	fmt.Printf("  %ssectionsInfo%s   - Display detailed section information.\n", green, reset)
+	fmt.Printf("  %sstrings-info%s   - List string-based dynamic tags.\n", green, reset)
+	fmt.Printf("  %svalues-info%s    - List numeric-based dynamic tags.\n", green, reset)
 
 	fmt.Printf("\n%sExample:%s\n", yellow, reset)
 	fmt.Printf("  %s./elfutils.exe%s %smyfile.elf%s %ssections%s\n", cyan, reset, green, reset, red, reset)
@@ -183,6 +214,49 @@ func main() {
 		help()
 		return
 	}
+	
+	valueTags := []elf.DynTag{
+		elf.DT_PLTRELSZ, elf.DT_SYMTAB, elf.DT_RELA, elf.DT_INIT,
+		elf.DT_FINI, elf.DT_TEXTREL, elf.DT_JMPREL, elf.DT_GNU_HASH,
+		elf.DT_NULL, elf.DT_NEEDED, elf.DT_PLTGOT, elf.DT_HASH,
+		elf.DT_STRTAB, elf.DT_SYMTAB, elf.DT_RELA, elf.DT_RELASZ,
+		elf.DT_RELAENT, elf.DT_STRSZ, elf.DT_SYMENT, elf.DT_INIT,
+		elf.DT_FINI, elf.DT_SONAME, elf.DT_RPATH, elf.DT_SYMBOLIC,
+		elf.DT_REL, elf.DT_RELSZ, elf.DT_RELENT, elf.DT_PLTREL,
+		elf.DT_DEBUG, elf.DT_TEXTREL, elf.DT_JMPREL, elf.DT_BIND_NOW,
+		elf.DT_INIT_ARRAY, elf.DT_FINI_ARRAY, elf.DT_INIT_ARRAYSZ,
+		elf.DT_FINI_ARRAYSZ, elf.DT_RUNPATH, elf.DT_FLAGS, elf.DT_ENCODING,
+		elf.DT_PREINIT_ARRAY, elf.DT_PREINIT_ARRAYSZ, elf.DT_SYMTAB_SHNDX,
+		elf.DT_LOOS, elf.DT_HIOS, elf.DT_VALRNGLO, elf.DT_GNU_PRELINKED,
+		elf.DT_GNU_CONFLICTSZ, elf.DT_GNU_LIBLISTSZ, elf.DT_CHECKSUM,
+		elf.DT_PLTPADSZ, elf.DT_MOVEENT, elf.DT_MOVESZ, elf.DT_FEATURE,
+		elf.DT_POSFLAG_1, elf.DT_SYMINSZ, elf.DT_SYMINENT, elf.DT_VALRNGHI,
+		elf.DT_ADDRRNGLO, elf.DT_GNU_HASH, elf.DT_TLSDESC_PLT, elf.DT_TLSDESC_GOT,
+		elf.DT_GNU_CONFLICT, elf.DT_GNU_LIBLIST, elf.DT_CONFIG, elf.DT_DEPAUDIT,
+		elf.DT_AUDIT, elf.DT_PLTPAD, elf.DT_MOVETAB, elf.DT_SYMINFO,
+		elf.DT_ADDRRNGHI, elf.DT_VERSYM, elf.DT_RELACOUNT, elf.DT_RELCOUNT,
+		elf.DT_FLAGS_1, elf.DT_VERDEF, elf.DT_VERDEFNUM, elf.DT_VERNEED,
+		elf.DT_VERNEEDNUM, elf.DT_LOPROC, elf.DT_MIPS_RLD_VERSION, elf.DT_MIPS_TIME_STAMP,
+		elf.DT_MIPS_ICHECKSUM, elf.DT_MIPS_IVERSION, elf.DT_MIPS_FLAGS, elf.DT_MIPS_BASE_ADDRESS,
+		elf.DT_MIPS_MSYM, elf.DT_MIPS_CONFLICT, elf.DT_MIPS_LIBLIST, elf.DT_MIPS_LOCAL_GOTNO,
+		elf.DT_MIPS_CONFLICTNO, elf.DT_MIPS_LIBLISTNO, elf.DT_MIPS_SYMTABNO, elf.DT_MIPS_UNREFEXTNO,
+		elf.DT_MIPS_GOTSYM, elf.DT_MIPS_HIPAGENO, elf.DT_MIPS_RLD_MAP, elf.DT_MIPS_DELTA_CLASS,
+		elf.DT_MIPS_DELTA_CLASS_NO, elf.DT_MIPS_DELTA_INSTANCE, elf.DT_MIPS_DELTA_INSTANCE_NO,
+		elf.DT_MIPS_DELTA_RELOC, elf.DT_MIPS_DELTA_RELOC_NO, elf.DT_MIPS_DELTA_SYM,
+		elf.DT_MIPS_DELTA_SYM_NO, elf.DT_MIPS_DELTA_CLASSSYM, elf.DT_MIPS_DELTA_CLASSSYM_NO,
+		elf.DT_MIPS_CXX_FLAGS, elf.DT_MIPS_PIXIE_INIT, elf.DT_MIPS_SYMBOL_LIB, elf.DT_MIPS_LOCALPAGE_GOTIDX,
+		elf.DT_MIPS_LOCAL_GOTIDX, elf.DT_MIPS_HIDDEN_GOTIDX, elf.DT_MIPS_PROTECTED_GOTIDX, elf.DT_MIPS_OPTIONS,
+		elf.DT_MIPS_INTERFACE, elf.DT_MIPS_DYNSTR_ALIGN, elf.DT_MIPS_INTERFACE_SIZE, elf.DT_MIPS_RLD_TEXT_RESOLVE_ADDR,
+		elf.DT_MIPS_PERF_SUFFIX, elf.DT_MIPS_COMPACT_SIZE, elf.DT_MIPS_GP_VALUE, elf.DT_MIPS_AUX_DYNAMIC,
+		elf.DT_MIPS_PLTGOT, elf.DT_MIPS_RWPLT, elf.DT_MIPS_RLD_MAP_REL, elf.DT_PPC_GOT, elf.DT_PPC_OPT,
+		elf.DT_PPC64_GLINK, elf.DT_PPC64_OPD, elf.DT_PPC64_OPDSZ, elf.DT_PPC64_OPT, elf.DT_SPARC_REGISTER,
+		elf.DT_AUXILIARY, elf.DT_USED, elf.DT_FILTER, elf.DT_HIPROC,
+	}
+	
+	
+	stringTags := []elf.DynTag{
+		elf.DT_NEEDED, elf.DT_SONAME, elf.DT_RPATH, elf.DT_RUNPATH,
+	}	
 
 	fileName := os.Args[1]
 	command := os.Args[2]
@@ -263,8 +337,15 @@ func main() {
 		result = relocs(elfFile)
 	case "sectionsInfo":
 		result = sectionsInfo(elfFile)
+		
+	case "strings-info":
+		result = processStringTags(elfFile, stringTags)
+		
+	case "values-info":
+		result = processValueTags(elfFile, valueTags)
+	
 	default:
-		fmt.Println("Unknown command. Valid commands are: dwarf, lib, sections, sym, class, machine, entryPoint, fileHeader, stringTable, dynamicSymbols, relocs, sectionsInfo")
+		fmt.Println("Unknown command. Valid commands are: dwarf, lib, sections, sym, class, machine, entryPoint, fileHeader, stringTable, dynamicSymbols, relocs, strings-info, sectionsInfo, values-info")
 		help()
 		return
 	}
