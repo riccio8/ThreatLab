a54891dc20777e2fa1e0b59deff291ca6da98050
0x90 <134144784+riccio8@users.noreply.github.com>
2025-04-18 21:36:17 +0200

    Update main.rs

--- src/web/WordV/src/main.rs
+++ src/web/WordV/src/main.rs
@@ -1,6 +1,9 @@
 use reqwest::Client;
 use std::error::Error;
 
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
+
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt; // for write_all()
 
@@ -14,17 +17,27 @@ async fn main() -> Result<(), Box<dyn Error>> {
     
     if response.status().is_success() {
         let body = response.text().await?; 
-        save_feed(body).await?;    
+        save_feed(&body).await?;    
+        println!("File feed.xml saved successfully");
+        find(&body);
     } else {
-        eprintln!("Errore nella richiesta: {}", response.status());
+        eprintln!("Error in request: \n {} \n", response.status());
     }
-
+    // parser("feed.xml".to_string())?;
     Ok(())
 }
-
-async fn save_feed(text: String) -> Result<(),Box<dyn Error>>{
+async fn save_feed(text: &String) -> Result<(),Box<dyn Error>>{
     let mut file = File::create("feed.xml").await?;
     file.write_all(text.as_bytes()).await?;
 
     Ok(())
+}
+
+
+fn find(text: &String){
+    for line in text.lines(){
+        if line.contains("generator"){
+            println!("Version found at line {}", line);
+        }
+    }
 }
